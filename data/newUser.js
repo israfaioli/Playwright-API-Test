fcfa4082f970bf7ae881c4af136cd80d55358333
Israel Faioli <68829837+israfaioli@users.noreply.github.com>
2023-09-12 15:22:15 -0300

    fix: update email faker.number variable

--- data/newUser.js
+++ data/newUser.js
@@ -4,6 +4,6 @@ export const newUser = {
     username: faker.person.middleName() + faker.number.int(1000),
     first_name: faker.person.firstName(),
     last_name: faker.person.lastName(),
-    email: faker.person.middleName() + 'API@gmai.com',
+    email: faker.person.middleName() + faker.number.int(1000) + 'API@gmai.com',
     password: "APIrest#1234"
 }
